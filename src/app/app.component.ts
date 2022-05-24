259bd3b08f7ea9c09a8f8afb3d4980d4cc97bddb
hugobarbosa <hugodu38@icloud.com>
2022-05-24 12:51:52 +0200

    Exercice 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,17 @@ import {Statistique} from "./models/statistique";
 })
 export class AppComponent {
   title = 'stat';
-  statistique1: Statistique = new Statistique( "ronaldo", "12", "448 471 000");
-  statistique2: Statistique = new Statistique( "messi", "11", "406 632 000");
+  statistiques: Statistique[] = [
+    new Statistique("ronaldo", "12", "777 111 000"),
+    new Statistique("messi", "11", "999 444 777")];
+
+  constructor(){
+    setTimeout(() => {
+      this.statistiques.push(new Statistique("Neymar", "4", "000 111 000"));
+    }, 4000);
+  }
+
+
 
 }
 
