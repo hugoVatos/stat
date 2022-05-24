fa751f1bf41a74b145eba8f8c768fb6a212d8b98
hugobarbosa <hugodu38@icloud.com>
2022-05-24 12:04:52 +0200

    exercice 3

---
+++ src/app/models/statistique.ts
@@ -0,0 +1,11 @@
+export class Statistique {
+
+  libelle? : string;
+  pourcent? : string;
+
+  constructor(libelle : string, pourcent : string) {
+    this.libelle = libelle;
+    this.pourcent = pourcent;
+  }
+}
+
