fec9ad6bb40911030000d7b1b5ced243b50d688f
hugobarbosa <hugodu38@icloud.com>
2022-05-24 12:44:15 +0200

    Exercice 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { StatistiqueComponent } from './statistique/statistique.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatistiqueComponent
   ],
   imports: [
     BrowserModule,
