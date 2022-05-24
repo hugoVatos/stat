849be3dcff1f52c653d721b5ca0336ef01942805
hugobarbosa <hugodu38@icloud.com>
2022-05-24 12:31:46 +0200

    Exercice 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Statistique} from "./models/statistique";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stat';
+  statistique1: Statistique = new Statistique( "ronaldo", "12", "448 471 000");
+  statistique2: Statistique = new Statistique( "messi", "11", "406 632 000");
+
 }
+
--- src/app/models/statistique.ts
+++ src/app/models/statistique.ts
@@ -2,10 +2,13 @@ export class Statistique {
 
   libelle? : string;
   pourcent? : string;
+  code?: string;
 
-  constructor(libelle : string, pourcent : string) {
+  constructor(libelle : string, pourcent : string, code : string) {
     this.libelle = libelle;
     this.pourcent = pourcent;
+    this.code = code;
   }
 }
+
 
